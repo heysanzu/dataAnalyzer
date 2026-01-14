d53d7e2b21f31838210f3ea363ab7b7a577b0367
Shahnewaz Alam <181187140+heysanzu@users.noreply.github.com>
2026-01-14 17:10:54 +0530

    Create dataAnalyzer.py

---
+++ dataAnalyzer.py
@@ -0,0 +1,6 @@
+# Create data as a list of dictionaries
+data = [
+    {'Name': 'Sanzu', 'Age': 20, 'Score': 99},
+    {'Name': 'Bob', 'Age': 25, 'Score': 90},
+    {'Name': 'Charlie', 'Age': 23, 'Score': 88}
+]
